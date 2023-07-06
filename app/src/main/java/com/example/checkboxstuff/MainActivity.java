1a5eb4e08bc49b68c76425eee44a9644eafbdd26
Miguel Lofredo <miguellofredo@gmail.com>
2023-07-06 16:41:35 -0300

    radios playing toasting

--- app/src/main/java/com/example/checkboxstuff/MainActivity.java
+++ app/src/main/java/com/example/checkboxstuff/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,26 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
         CheckBox ch1, ch2;
         Button button;
-        ch1 = findViewById(R.id.checkBox);
-        ch2 = findViewById(R.id.checkBox2);
-        button = findViewById(R.id.button);
-        String textCh1 = ch1.getText().toString();
-        String textCh2 = ch2.getText().toString();
+        RadioGroup radioGroup;
+        ch1        = findViewById(R.id.checkBox);
+        ch2        = findViewById(R.id.checkBox2);
+        button     = findViewById(R.id.button);
+        radioGroup = findViewById(R.id.radioGroup);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(ch1.isChecked() && ch2.isChecked()){
-                    Toast.makeText(MainActivity.this, textCh1 + " " +textCh2, Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, ch1.getText() + " " +ch2.getText(), Toast.LENGTH_LONG).show();
                 } else if (ch2.isChecked()) {
-                    Toast.makeText(MainActivity.this, textCh2, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, ch2.getText(), Toast.LENGTH_SHORT).show();
                 } else if (ch1.isChecked()) {
-                    Toast.makeText(MainActivity.this, textCh1, Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, ch1.getText(), Toast.LENGTH_LONG).show();
 
                 } else{
                     Toast.makeText(MainActivity.this, "Nothing selected", Toast.LENGTH_LONG).show();
                 }
+
+            radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup radioGroup, int checked) {
+                    RadioButton radioButton = findViewById(checked);
+                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_LONG).show();
+                }
+            });
             }
+
         });
     }
 
